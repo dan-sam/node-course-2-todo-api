4cd317023f7e8fe0607b0fa9ea43704b8ce56548
Danijel Veljkovic <vedaniel.eth@gmail.com>
2018-10-02 14:24:19 +0200

    Add test cases for GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -22,6 +22,7 @@ app.post('/todos', (req, res) => {
   });
 });
 
+
 app.get('/todos', (req, res) => {
   Todo.find().then((todos)=>{
     res.send({todos});
@@ -30,12 +31,13 @@ app.get('/todos', (req, res) => {
   });
 });
 
+
+
+
 app.get('/todos/:id', (req, res) => {
 
   var id = req.params.id;
   //res.send(req.params);
-
-
     if(!ObjectID.isValid(id)){
       return res.status(404).send();
     }
@@ -51,6 +53,9 @@ app.get('/todos/:id', (req, res) => {
     });
 
 });
+
+
+
 
 app.listen(3000, () => {
   console.log("Started on port 3000");

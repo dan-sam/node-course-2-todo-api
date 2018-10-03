6b04241f0bbcbc56973084dbd864e437b1566d54
Danijel Veljkovic <vedaniel.eth@gmail.com>
2018-10-03 21:22:32 +0200

    Add PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,10 +1,12 @@
-var express = require("express");
-var bodyParser = require("body-parser");
+const _ = require('lodash');
+const express = require("express");
+const bodyParser = require("body-parser");
+const {ObjectID} = require('mongodb');
 
 var {mongoose} = require("./db/mongoose.js");
 var {Todo} = require("./models/todo.js");
 var {User} = require("./models/user.js");
-const {ObjectID} = require('mongodb');
+
 
 var app = express();
 const port = process.env.PORT || 3000;
@@ -59,16 +61,16 @@ app.get('/todos/:id', (req, res) => {
 
 app.delete('/todos/:id', (req, res)=>{
   //get the id
-  var IdToDelete = req.params.id;
+  var id = req.params.id;
 
   // validate the id => not valid? return 404
-  if(!ObjectID.isValid(IdToDelete)){
+  if(!ObjectID.isValid(id)){
     return res.status(404).send();
   }
   //remove todo by id
     //success => if no doc, send 404 | if doc found, send 200
     //error => 400 with empty body
-  Todo.findByIdAndRemove(IdToDelete).then((todo) => {
+  Todo.findByIdAndRemove(id).then((todo) => {
     if(!todo){
       return res.status(404).send();
     }
@@ -78,6 +80,41 @@ app.delete('/todos/:id', (req, res)=>{
     });
 });
 
+
+app.patch('/todos/:id', (req, res) => {
+  var id = req.params.id;
+
+  //pick fields from the request body if they exist
+  var body = _.pick(req.body, ['text', 'completed']);
+
+  // validate the id => not valid? return 404
+  if(!ObjectID.isValid(id)){
+    return res.status(404).send();
+  }
+
+  if(_.isBoolean(body.completed) && body.completed){
+    body.completedAt = new Date().getTime();
+  }
+  else{
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  //Update uses certail sets of parameters, e.g. $set, $inc, just like we did
+  //in mongodb-update.js
+
+  Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+    if(!todo){
+      return res.status(404).send();
+    }
+
+    res.status(200).send({todo});
+
+  }).catch((err) => {
+    res.status(400).send();
+  });
+
+});
 
 
 
